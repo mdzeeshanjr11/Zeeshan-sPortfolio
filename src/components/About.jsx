237d204a19e0735fbff1acd27e0c6b4938ca48eb
mdzeeshanjr11 <mdzeeshanjr11@gmail.com>
2026-09-07 17:22:03 +0530

    1.2

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -27,7 +27,7 @@ function Hero() {
             </div>
 
             <h1 className="hero-name">
-             Mohammed  <span className="grad">Zeshan</span>
+             Mohammed  <span className="grad">Zeeshan</span>
             </h1>
 
             <div className="hero-role">
